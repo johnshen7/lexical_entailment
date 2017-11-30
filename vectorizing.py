ec05683c5a691d89c8ae0a31913f9adc8db9f766
xususan <susan.xu97@gmail.com>
2017-11-30 01:57:51 -0500

    include validation set

--- vectorizing.py
+++ vectorizing.py
@@ -5,16 +5,16 @@ import numpy as np
 # Load Google's pre-trained Word2Vec model.
 model = gensim.models.KeyedVectors.load_word2vec_format('vectors/GoogleNews-vectors-negative300.bin', binary=True)  
 
-# Train
-train = pd.read_csv('lexical_entailment/bless2011/data_lex_train.tsv', sep='\t', header=None)
+# Dataset to vectorize
+df = pd.read_csv('lexical_entailment/bless2011/data_lex_val.tsv', sep='\t', header=None)
 
 # Test
 # test = pd.read_csv('lexical_entailment/bless2011/data_lex_test.tsv', sep='\t', header=None)
 
 # Replace cols 1 and 2 with their vectors using the model and gensim
-words_0 = train[0]
-words_1 = train[1]
-y = train[[2]]
+words_0 = df[0]
+words_1 = df[1]
+y = df[[2]]
 
 def vectorize_word(word):
 	if word in model.wv:
@@ -26,4 +26,4 @@ vectors_0 = words_0.apply(vectorize_word)
 vectors_1 = words_1.apply(vectorize_word)
 
 vectors_x = pd.concat([vectors_0, vectors_1, y], axis = 1)
-vectors_x.to_csv('lexical_entailment/bless2011/data_lex_test_vectorized.tsv', sep='\t', header=False, index=False)
+vectors_x.to_csv('lexical_entailment/bless2011/data_lex_val_vectorized.tsv', sep='\t', header=False, index=False)
