f5dfb9108b49ca8be76d525f00a763595def5750
xususan <susan.xu97@gmail.com>
2017-11-30 01:57:51 -0500

    include validation set

--- test_svm.py
+++ test_svm.py
@@ -5,20 +5,24 @@ from sklearn.metrics import accuracy_score
 clf = joblib.load('models/svm.pkl') 
 
 test_vectorized = pd.read_csv('lexical_entailment/bless2011/data_lex_test_vectorized.tsv', sep='\t', header=None)
-orig_rows, orig_cols = test_vectorized.shape
+val_vectorized = pd.read_csv('lexical_entailment/bless2011/data_lex_val_vectorized.tsv', sep='\t', header=None)
 
-# Remove rows with NaN
-test_vectorized.dropna(axis=0, inplace=True)
+# Test and validation bc i'm lazy
+for test_name, test_df in zip(['test', 'val'], [test_vectorized, val_vectorized]):
+	orig_rows, orig_cols = test_df.shape
 
-# Count number of rows removed
-diff = orig_rows - test_vectorized.shape[0]
+	# Remove rows with NaN
+	test_df.dropna(axis=0, inplace=True)
 
-X = test_vectorized.iloc[:, :-1]
-y = test_vectorized.iloc[:, -1]
+	# Count number of rows removed
+	diff = orig_rows - test_df.shape[0]
 
-preds = clf.predict(X)
+	X = test_df.iloc[:, :-1]
+	y = test_df.iloc[:, -1]
 
-num_correct = accuracy_score(y, preds, normalize=False)
+	preds = clf.predict(X)
 
-print "percentage non-nan correct:", num_correct/float(test_vectorized.shape[0]) 
-print "percentage correct overall", num_correct/float(orig_rows)
+	num_correct = accuracy_score(y, preds, normalize=False)
+
+	print test_name, ": percentage non-nan correct:", num_correct/float(test_df.shape[0]) 
+	print test_name, ": percentage correct overall", num_correct/float(orig_rows)
