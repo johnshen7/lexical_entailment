2064fcd666cfa3265f7f75e69997d4ce7a09431e
xususan <susan.xu97@gmail.com>
2017-11-30 10:19:51 -0500

    added another file

--- train_asym.py
+++ train_asym.py
@@ -2,7 +2,7 @@ from sklearn import svm
 from sklearn.externals import joblib
 import pandas as pd
 # Open vectorized training file
-train = pd.read_csv('lexical_entailment/bless2011/data_lex_train_vectorized.tsv', sep='\t', header=None)
+train = pd.read_csv('lexical_entailment/bless2011/data_lex_train_vectorized_asym.tsv', sep='\t', header=None)
 
 # Remove NaN
 train.dropna(axis=0, inplace=True)
@@ -12,4 +12,4 @@ y = train.iloc[:, -1]
 clf = svm.SVC()
 clf.fit(X, y)
 
-joblib.dump(clf, 'models/svm.pkl') 
+joblib.dump(clf, 'models/svm_asym.pkl') 
