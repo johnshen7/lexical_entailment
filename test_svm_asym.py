4395ef752c2349012fc6aa787733eb8cc5fa7fea
John Shen <jshen@college.harvard.edu>
2017-11-30 15:39:09 -0500

    Testing

--- test_svm_asym.py
+++ test_svm_asym.py
@@ -2,13 +2,16 @@ from sklearn.externals import joblib
 import pandas as pd
 from sklearn.metrics import accuracy_score
 
-clf = joblib.load('models/svm_asym.pkl') 
-
+clf = joblib.load('models/svm_asym.pkl')
+test = pd.read_csv('lexical_entailment/bless2011/data_lex_test.tsv', sep='\t', header=None)
 test_vectorized = pd.read_csv('lexical_entailment/bless2011/data_lex_test_vectorized_asym.tsv', sep='\t', header=None)
 val_vectorized = pd.read_csv('lexical_entailment/bless2011/data_lex_val_vectorized_asym.tsv', sep='\t', header=None)
 
-# Test and validation in one go bc i'm lazy
-for test_name, test_df in zip(['test', 'val'], [test_vectorized, val_vectorized]):
+def evaluate(test_name):
+	if test_name == 'val':
+		test_df = val_vectorized
+	else:
+		test_df = test_vectorized
 	orig_rows, orig_cols = test_df.shape
 
 	# Remove rows with NaN
@@ -22,7 +25,17 @@ for test_name, test_df in zip(['test', 'val'], [test_vectorized, val_vectorized]
 
 	preds = clf.predict(X)
 
+	true_count = 0
+	for p in y:
+		if p:
+			true_count += 1
+	print true_count, orig_rows
+
 	num_correct = accuracy_score(y, preds, normalize=False)
 
-	print test_name, ": percentage non-nan correct:", num_correct/float(test_df.shape[0]) 
+
+
+	print test_name, ": percentage non-nan correct:", num_correct/float(test_df.shape[0])
 	print test_name, ": percentage correct overall", num_correct/float(orig_rows)
+
+evaluate('test')
