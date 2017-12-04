4497a053905b4577b96f924354085e8772a62f80
Tiffany Yu <tiffanyyu268@gmail.com>
2017-12-04 16:43:04 -0500

    attempting russian

---
+++ russian/logreg_concat.py
@@ -0,0 +1,44 @@
+from sklearn.linear_model import LogisticRegression as LogReg
+from sklearn.cross_validation import train_test_split
+import sklearn.metrics as metrics
+import pandas as pd
+
+### Training
+# Open vectorized training file
+df = pd.read_csv('lexical_entailment/russian/lrwc_vectorized.tsv', sep='\t', header=None)
+
+train, test = train_test_split(df.values)
+print train.shape, test.shape
+train = pd.DataFrame(train)
+test = pd.DataFrame(test)
+
+# Remove NaN
+train.dropna(axis=0, inplace=True)
+X = train.iloc[:, :600]
+y = train.iloc[:, -1].astype(bool)
+
+clf = LogReg()
+clf.fit(X, y)
+
+### Testing
+orig_rows, orig_cols = test.shape
+
+# Remove rows with NaN
+test.dropna(axis=0, inplace=True)
+
+# Count number of rows removed
+diff = orig_rows - test.shape[0]
+
+X = test.iloc[:, :600]
+y = test.iloc[:, -1].astype(bool)
+
+preds = clf.predict(X)
+
+print "precision", metrics.precision_score(y, preds)
+print "recall", metrics.recall_score(y, preds)
+print "f1", metrics.f1_score(y, preds)
+
+num_correct = metrics.accuracy_score(y, preds, normalize=False)
+
+print "test : percentage non-nan correct:", num_correct/float(test.shape[0]) 
+print "test : percentage correct overall", num_correct/float(orig_rows)
