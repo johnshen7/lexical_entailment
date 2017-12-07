21729697413d438da30074a3d5a550a16fa2a4bd
xususan <susan.xu97@gmail.com>
2017-12-07 16:01:08 -0500

    english lol

---
+++ english/hfeature.py
@@ -0,0 +1,134 @@
+from sklearn.linear_model import LogisticRegression as LogReg
+from sklearn.cross_validation import train_test_split
+from sklearn import svm
+from sklearn.externals import joblib
+import sklearn.metrics as metrics
+import pandas as pd
+import numpy as np
+
+# Open vectorized files
+train = pd.read_csv('../datasets/bless2011/data_lex_train_vectorized_diff.tsv', sep='\t', header=None)
+test = pd.read_csv('../datasets/bless2011/data_lex_test_vectorized_diff.tsv', sep='\t', header=None)
+
+### Training
+train.dropna(axis=0, inplace=True)
+X = train.iloc[:, :600]
+y = train.iloc[:, -1].astype(bool)
+
+H = train.iloc[:, :300]
+w = train.iloc[:, 300:600]
+
+iterations = 2
+
+feature_vector = pd.DataFrame()
+
+for _ in range(iterations):
+    # Fit log reg
+    clf = LogReg()
+    clf.fit(X, y)
+    print "Log reg fit"
+
+    # Decision plane -- "H-feature detector"
+    p = np.array([clf.coef_[0][300:600]])
+    print "found p"
+
+    # Generate feature vector
+    # cos(H, w)
+    hw_sim = pd.DataFrame(np.dot(H.values, w.values.T).diagonal())
+    print "hw done"
+    # cos(H, p)
+    hp_sim = pd.DataFrame(np.dot(H.values, p.T))
+    print "hp done"
+    # cos(w, p)
+    wp_sim = pd.DataFrame(np.dot(w.values, p.T))
+    print "wp done"
+    # cos((H - w), p)
+    hwp_sim = pd.DataFrame(np.dot((H.values - w.values), p.T))
+    print "hwp done"
+
+    # Union with previous feature vectors
+    new_df = pd.concat([feature_vector, hw_sim, hp_sim, wp_sim, hwp_sim], axis = 1)
+    print "new df dims:", new_df.shape
+    feature_vector = new_df
+
+    # Generate new <H, w> pairs
+    h_proj = np.dot(np.dot(H, p.T / p.sum()), p)
+    h_rej = np.array(H - h_proj)
+    h_rej_norm = h_rej / h_rej.sum(axis=1).reshape(h_rej.shape[0], -1)
+    H = pd.DataFrame(h_rej_norm)
+    print H.shape
+    w_proj = np.dot(np.dot(w, p.T / p.sum()), p)
+    w_rej = np.array(w - w_proj)
+    w_rej_norm = w_rej / w_rej.sum(axis=1).reshape(w_rej.shape[0], -1)
+    w = pd.DataFrame(w_rej_norm)
+    print w.shape
+    X = pd.concat([H, w], axis = 1)
+
+print X.shape
+# Use SVM on feature vectors for final classifier
+final_clf = svm.SVC(class_weight='auto')
+final_clf.fit(feature_vector, y)
+print "Training complete"
+
+### Testing
+orig_rows, orig_cols = test.shape
+
+# Remove rows with NaN
+test.dropna(axis=0, inplace=True)
+
+# Count number of rows removed
+diff = orig_rows - test.shape[0]
+
+X = test.iloc[:, :-1]
+y = test.iloc[:, -1].astype(bool)
+
+H = test.iloc[:, :300]
+w = test.iloc[:, 300:600]
+
+feature_vector = pd.DataFrame()
+
+for _ in range(iterations):
+    # Fit log reg
+    h_clf = LogReg()
+    h_clf.fit(X, y)
+    print "Log reg fit"
+
+    # Decision plane -- "H-feature detector"
+    p = np.array([h_clf.coef_[0][300:600]])
+
+    # Generate feature vector
+    # cos(H, w)
+    hw_sim = pd.DataFrame(np.dot(H.values, w.values.T).diagonal())
+    # cos(H, p)
+    hp_sim = pd.DataFrame(np.dot(H.values, p.T))
+    # cos(w, p)
+    wp_sim = pd.DataFrame(np.dot(w.values, p.T))
+    # cos((H - w), p)
+    hwp_sim = pd.DataFrame(np.dot((H.values - w.values), p.T))
+
+    # Union with previous feature vectors
+    new_df = pd.concat([feature_vector, hw_sim, hp_sim, wp_sim, hwp_sim], axis = 1)
+    print "new df dims:", new_df.shape
+    feature_vector = new_df
+
+    # Generate new <H, w> pairs
+    h_proj = np.dot(np.dot(H, p.T / p.sum()), p)
+    h_rej = np.array(H - h_proj)
+    h_rej_norm = h_rej / h_rej.sum(axis=1).reshape(h_rej.shape[0], -1)
+    H = pd.DataFrame(h_rej_norm)
+    w_proj = np.dot(np.dot(w, p.T / p.sum()), p)
+    w_rej = np.array(w - w_proj)
+    w_rej_norm = w_rej / w_rej.sum(axis=1).reshape(w_rej.shape[0], -1)
+    w = pd.DataFrame(w_rej_norm)
+    X = pd.concat([H, w], axis = 1)
+
+preds = final_clf.predict(feature_vector)
+
+print "precision", metrics.precision_score(y, preds)
+print "recall", metrics.recall_score(y, preds)
+print "f1", metrics.f1_score(y, preds)
+
+num_correct = metrics.accuracy_score(y, preds, normalize=False)
+
+print "test : percentage non-nan correct:", num_correct/float(test.shape[0])
+print "test : percentage correct overall", num_correct/float(orig_rows)
---
+++ english/vectorizing.py
@@ -0,0 +1,45 @@
+import gensim
+import pandas as pd
+import numpy as np
+import sys
+from sklearn import preprocessing
+
+""" USAGE: python vectorizing.py path/to/dataset.tsv path/to/vectorized method
+path/to/dataset.tsv: dataset to model vectors, assumes the last column has prediction. default is datasets/russian/lrwc_vectorized.tsv
+path/to/vectorized: desired output location
+method: diff, asym
+
+Example: python vectorizing.py default datasets/russian/lrwc_vectorized_diff.tsv diff
+"""
+if len(sys.argv) != 4:
+    raise ValueError('Usage: python vectorizing.py path/to/model.bin path/to/dataset.tsv path/to/vectorized method')
+
+path_to_dataset = '../datasets/russian/lrwc_vectorized.tsv' if sys.argv[1] == 'default' else sys.argv[1]
+path_to_vectorized = sys.argv[2]
+method = sys.argv[3]
+
+df = pd.read_csv(path_to_dataset, sep='\t', header=None)
+
+vectors_0 = df.iloc[:, :300]
+vectors_1 = df.iloc[:, 300:600]
+y = df.iloc[:, -1]
+
+def merge_vectors(v1, v2, method):
+    print "method", method
+    if method == 'concat':
+        return pd.concat([v1, v2, y], axis = 1)
+    elif method == 'diff':
+        # Roller 2014 says to normalize the difference
+        diff = v1.values - v2.values
+        print diff.shape
+        return pd.concat([pd.DataFrame(diff), y], axis = 1)
+    elif method == 'asym':
+        # diff
+        a = v1.values - v2.values
+        # squared diff
+        b = pd.DataFrame(np.sqrt(np.square(a).sum(axis=1)))
+        return pd.concat([pd.DataFrame(a), pd.DataFrame(b), y], axis = 1)
+
+vectors_x = merge_vectors(vectors_0, vectors_1, method)
+print vectors_x.shape
+vectors_x.to_csv(path_to_vectorized, sep='\t', header=False, index=False)
